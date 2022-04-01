e7cc8748a5036b7de8685e4c871235b9383bb0b8
Atena <91287064+Atenad86@users.noreply.github.com>
2022-04-01 13:21:02 +0430

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Document</title>
+    <?php require_once "Captcha.php"?>
+<?php
+
+if (isset($_POST['submit'])){
+    if (Captcha::checkCaptcha($_POST['captcha'])){
+        echo "yes";
+        header("location:index.php");
+    }else{
+        echo "wrong";
+    }
+}
+ ?>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <form class="mx-auto" method="post">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-group ">
+                <label for="exampleInputPassword1">Enter the Captcha</label>
+                <div class="d-flex">
+                    <input type="text" class="form-control mr-2" name="captcha" placeholder="captcha">
+                    <img src="captcha_add.php" alt="">
+                </div>
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
